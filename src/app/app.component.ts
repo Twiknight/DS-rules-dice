0f39dd213ff31242c0ba38b0d72c658b5a8e3861
unknown <twiknight>
2019-08-26 22:25:30 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { rules } from 'src/app/ds.rules'
+
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ds-rules-dice';
+  public allow_dup = false;
+  public use_ds_rules = false;
+
+  
+  private _results : string[];
+  public get results() : string[] {
+    return this._results;
+  }
+  public set results(v : string[]) {
+    this._results = v;
+  }
+
+  private pick_4() :string[] {
+    const keys = Object.keys(rules);
+    const rand = () => getRandomIntInclusive(0, 13)
+    if (this.allow_dup) {
+      return [keys[rand()], keys[rand()], keys[rand()], keys[rand()]]
+    } else {
+      return keys.sort(_ => 0.5 - Math.random()).slice(0, 4)
+    }
+  }
+
+  /**
+   * roll
+   */
+  public roll() {
+    if(this.use_ds_rules) {
+      this.results = this.pick_4().map(k => `${k}: ${rules[k]}`)
+    } else {
+      this.results = this.pick_4()
+    }
+  }
 }
